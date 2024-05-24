11e2e1419acc0fae840cf2a5ca22774e55f742d9
thefakehamma <hamma.khoshnaw@gmail.com>
2024-05-24 21:50:48 +0200

    First FE set up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold underline">Hello, world!</h1>
+    </div>
+  );
+}
+
+export default App;
