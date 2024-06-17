e5d0789fc3a7df87d88c03433d3b38de66e135fb
thefakehamma <hamma.khoshnaw@gmail.com>
2024-06-17 10:56:39 +0200

    update db

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const dbURI = process.env.MONGO_URL;
+    const dbURI = process.env.MONGO_URI;
     if (!dbURI) {
-      throw new Error('MONGO_URL environment variable is not set');
+      throw new Error('MONGO_URI environment variable is not set');
     }
     console.log('MongoDB URI:', dbURI);
     await mongoose.connect(dbURI, {
