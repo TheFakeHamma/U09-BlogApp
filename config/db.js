943fccd25d768686dfa8ddbe1438464cf99136e3
thefakehamma <hamma.khoshnaw@gmail.com>
2024-06-17 10:48:34 +0200

    Add debug log for MongoDB URI

--- config/db.js
+++ config/db.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    const dbURI = process.env.MONGO_URL;
-    await mongoose.connect(dbURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
+    try {
+        const dbURI = process.env.MONGO_URL;
+        console.log('MongoDB URI:', dbURI);
+        await mongoose.connect(dbURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB Connected...');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
