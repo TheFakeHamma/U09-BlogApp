a07fdc1847d9a40b23ccf23d4620c2910f79b4ee
thefakehamma <hamma.khoshnaw@gmail.com>
2024-06-17 11:28:50 +0200

    add url to app.js cors

--- app.js
+++ app.js
@@ -6,15 +6,25 @@ const path = require('path');
 
 dotenv.config();
 
+console.log('Environment Variables:', process.env);
+
 const app = express();
 connectDB();
 
-app.use(express.json({ extended: false }));
+const allowedOrigins = [process.env.FRONTEND_URL, 'https://schoolblogapp.netlify.app'];
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
 }));
+
+app.use(express.json({ extended: false }));
 
 // Define routes
 app.use('/api/users', require('./routes/userRoutes'));
