f10fb5f1fce1e1bed763cb44347d7ee321b7a9f0
thefakehamma <hamma.khoshnaw@gmail.com>
2024-06-17 10:04:08 +0200

    modify app.js for build

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 
 dotenv.config();
 
@@ -11,8 +12,16 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(cors());
 
+// Define routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/blogs', require('./routes/blogRoutes'));
+
+// Serve static files from React app
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
